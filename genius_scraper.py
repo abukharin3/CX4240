f490dea804097371f3604af03a9e5a69614a217d
abukharin3 <59148829+abukharin3@users.noreply.github.com>
2020-03-16 17:55:30 -0400

    Update genius_scraper.py

--- genius_scraper.py
+++ genius_scraper.py
@@ -5,37 +5,38 @@ class BrickSetSpider(scrapy.Spider):
     start_urls = ['https://genius.com/songs/all?page=1']#,'https://genius.com/songs/all?page=2']
 
     def song_parse(self, response):
-        print(response)
+        print("!!!!!!!!!!!!!!!!!!!!")
         SET_SELECTOR = 'p,lyrics ::text'
         print(response.css(SET_SELECTOR).extract_first())
+        #print()
 
-                
+        # with open("genius.text", "a") as file:
+        #     for song in response.css(SET_SELECTOR):
+        #         print("\n\n\n")
+        #         print(type(song))
+        #         print("\n\n\n")
+                #NAME_SELECTOR = 'p ::text'
+                #file.writelines(song.css(NAME_SELECTOR).extract())
+
 
     def parse(self, response):
-        with open("genius.text", "w") as file:
-            pass
-        
         song_list = []
 
         NEXT_PAGE_SELECTOR = 'a ::attr(href)'
         next_page = response.css(NEXT_PAGE_SELECTOR).extract()
-        print("\n\n\n")
-        #print(next_page)
+
 
         song_list = []
         for item in next_page:
             if item[-6:] == 'lyrics':
-                print(item)
                 song_list.append(item)
 
 
         print("\n\n\n")
         # Use top 50 songs
-        
-        for link in song_list[0 : 1]:
-            if song_list:
-                yield scrapy.Request(
-                    response.urljoin(link),
-                    callback=self.song_parse,
-                )
+        link = song_list[0]
+        yield scrapy.Request(
+            response.urljoin(link),
+            callback=self.song_parse,
+        )
         
