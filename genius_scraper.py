1437ab8c59bff088ce0ef182e390f024302573b7
mrepasky3 <mwrep@DESKTOP-QG4AEMN.localdomain>
2020-03-22 10:57:06 -0400

    replaced genius_scraper.py with Matt's version

--- genius_scraper.py
+++ genius_scraper.py
@@ -5,40 +5,40 @@ class BrickSetSpider(scrapy.Spider):
     start_urls = ['https://genius.com/songs/all?page=1']#,'https://genius.com/songs/all?page=2']
 
     def song_parse(self, response):
-        print("\n\n\n")
-        SET_SELECTOR = 'p,lyrics ::text'
-        print(response.css(SET_SELECTOR).extract_first())
-        print("\n\n\n")
-        #print()
+        print(response)
+        SET_SELECTOR = '.lyrics'
+        #print(response.css(SET_SELECTOR).extract_first())
+        with open("genius.text", "a") as file:
+            for song in response.css(SET_SELECTOR):
+                NAME_SELECTOR = 'p ::text'
+                file.writelines(song.css(NAME_SELECTOR).extract())
 
-        # with open("genius.text", "a") as file:
-        #     for song in response.css(SET_SELECTOR):
-        #         print("\n\n\n")
-        #         print(type(song))
-        #         print("\n\n\n")
-                #NAME_SELECTOR = 'p ::text'
-                #file.writelines(song.css(NAME_SELECTOR).extract())
-
+                
 
     def parse(self, response):
+        with open("genius.text", "w") as file:
+            pass
+        
         song_list = []
 
         NEXT_PAGE_SELECTOR = 'a ::attr(href)'
         next_page = response.css(NEXT_PAGE_SELECTOR).extract()
-
+        print("\n\n\n")
+        #print(next_page)
 
         song_list = []
         for item in next_page:
             if item[-6:] == 'lyrics':
+                print(item)
                 song_list.append(item)
 
 
         print("\n\n\n")
         # Use top 50 songs
+        
         for link in song_list[2 : 3]:
             if song_list:
                 yield scrapy.Request(
                     response.urljoin(link),
                     callback=self.song_parse,
                 )
-
