77b3fe2a26440af8f7be6bc0eaa3cb5d1aea83a9
mrepasky3 <mwrep@DESKTOP-QG4AEMN.localdomain>
2020-03-22 12:28:30 -0400

    added genre scrapers, removed misc testing files/folders

---
+++ rap_scraper.py
@@ -0,0 +1,38 @@
+import scrapy
+
+class BrickSetSpider(scrapy.Spider):
+    name = "brickset_spider"
+    start_urls = ['https://genius.com/tags/rap/all?page=' + str(i) for i in range(1, 20)]#,'https://genius.com/songs/all?page=2']
+
+    def song_parse(self, response):
+        SET_SELECTOR = '.lyrics'
+        with open("rap_songs.text", "a") as file:
+            for song in response.css(SET_SELECTOR):
+                NAME_SELECTOR = 'p ::text'
+                file.writelines(["\n\n\nStart of song \n\n\n"])
+                file.writelines(song.css(NAME_SELECTOR).extract())
+
+
+
+    def parse(self, response):
+        with open("rap_songs.text", "w") as file:
+            pass
+
+        song_list = []
+
+        NEXT_PAGE_SELECTOR = 'a ::attr(href)'
+        next_page = response.css(NEXT_PAGE_SELECTOR).extract()
+
+        song_list = []
+        for item in next_page:
+            if item[-6:] == 'lyrics':
+                print(item)
+                song_list.append(item)
+
+
+        for link in song_list:
+            if song_list:
+                yield scrapy.Request(
+                    response.urljoin(link),
+                    callback=self.song_parse,
+                )
