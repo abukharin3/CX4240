Fixed error in loss calculation

MLP.py
@@ -10,11 +10,11 @@ class MLP(Model):
     def __init__(self, train, train_labels, test, test_labels):
         super(MLP, self).__init__()
         # Define training parameters
-        self.learning_rate = 0.01
+        self.learning_rate = 0.001
         self.training_steps = 20000
-        self.num_batches = 1000
-        self.display_step = 1000
-        self.num_epochs = 10
+        self.batch_size = 1000
+        self.display_step = 500
+        self.num_epochs = 3
         self.train = train
         self.train_labels = train_labels
         self.test = test
@@ -24,7 +24,7 @@ class MLP(Model):
         # Define hidden layers
         self.fc1 = layers.Dense(len(self.train[0]), activation = tf.nn.relu)
         self.fc2 = layers.Dense(256, activation = tf.nn.relu)
-        self.out = layers.Dense(self.num_genres, activation = tf.nn.relu)
+        self.out = layers.Dense(self.num_genres, activation = tf.nn.sigmoid)
         # Define Optimizer
         self.optimizer = tf.optimizers.Adam(self.learning_rate)
 
@@ -41,7 +41,7 @@ class MLP(Model):
     # Define Cross entropy loss
     def cross_entropy_loss(self, pred, y):
         loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels = y, logits = pred)
-        return tf.reduce_mean(loss)
+        return tf.reduce_mean(loss + 1e-3)
 
     def accuracy(self, y_pred, label):
         # Take argmax of prediction
@@ -67,16 +67,19 @@ class MLP(Model):
     def train_op(self):
         for e in range(self.num_epochs):
             self.shuffle()
-            for b in range(20):
-                batch_x = self.train[self.num_batches * b: self.num_batches * (b + 1)]
-                batch_y = self.train_labels[self.num_batches * b: self.num_batches * (b + 1)]
+            for b in range(4):
+                batch_x = self.train[self.batch_size * b: self.batch_size * (b + 1)]
+                batch_y = self.train_labels[self.batch_size * b: self.batch_size * (b + 1)]
                 for step in range(self.training_steps):
                     self.run_optimization(batch_x, batch_y)
                     if step % self.display_step == 0:
                         pred = self.call(batch_x, is_training = True)
                         loss = self.cross_entropy_loss(pred, batch_y)
                         acc = self.accuracy(pred, batch_y)
+                        test_pred = self.call(self.test, is_training = True)
+                        test_acc = self.accuracy(test_pred, self.test_labels)
                         print("step: %i, loss: %f, accuracy: %f, epoch: %f, batch: %f" % (step, loss, acc, e, b))
+                        print("Test Accuracy: %f" % (test_acc))
 
 
     def shuffle(self):

RNN.py
@@ -4,21 +4,24 @@ from tensorflow.keras import Model, layers
 import gc
 
 class LSTM(Model):
-    def __init__(self, train, train_labels):
+    def __init__(self, train, train_labels, test, test_labels):
         super(LSTM, self).__init__()
         # Define training parameters
-        self.learning_rate = 0.01
-        self.training_steps = 2000
-        self.num_batches = 50
-        self.display_step = 1
+        self.learning_rate = 0.001
+        self.training_steps = 1000
+        self.batch_size = 500
+        self.display_step = 10
         self.num_epochs = 3
         self.train = train
         self.train_labels = train_labels
+        self.test = test
+        self.test_labels = test_labels
 
         self.shuffle()
         self.num_genres = 5
         self.num_units = 32
         # Define hidden layers
+        self.fc1 = layers.Dense(len(self.train[0]), activation = tf.nn.relu)
         self.lstm_layer = layers.LSTM(units = self.num_units)
         self.out = layers.Dense(self.num_genres)
         # Define Optimizer
@@ -26,6 +29,7 @@ class LSTM(Model):
 
     def call(self, x, is_training = False):
         # Forward pass
+        x = self.fc1(x)
         x = self.lstm_layer(x)
         x = self.out(x)
         if not is_training:
@@ -62,21 +66,24 @@ class LSTM(Model):
 
     def train_op(self):
         for e in range(self.num_epochs):
-            order = self.shuffle()
-            for b in range(10):
-                batch_x = self.train[order][self.num_batches * b: self.num_batches * (b + 1)]
-                batch_y = self.train_labels[order][self.num_batches * b: self.num_batches * (b + 1)]
+            self.shuffle()
+            for b in range(8):
+                batch_x = self.train[self.batch_size * b: self.batch_size * (b + 1)]
+                batch_y = self.train_labels[self.batch_size * b: self.batch_size * (b + 1)]
                 for step in range(self.training_steps):
                     self.run_optimization(batch_x, batch_y)
                     if step % self.display_step == 0:
                         pred = self.call(batch_x, is_training = True)
                         loss = self.cross_entropy_loss(pred, batch_y)
                         acc = self.accuracy(pred, batch_y)
+                        test_pred = self.call(self.test, is_training = True)
+                        test_acc = self.accuracy(test_pred, self.test_labels)
                         print("step: %i, loss: %f, accuracy: %f, epoch: %f, batch: %f" % (step, loss, acc, e, b))
+                        print("Test Accuracy: %f" % (test_acc))
 
     def shuffle(self):
         train_order = np.random.permutation(len(self.train))
-        return train_order
+        self.train, self.train_labels = self.train[train_order], self.train_labels[train_order]
 
 
 
@@ -84,7 +91,11 @@ class LSTM(Model):
 # Load Data
 data = np.load("wordvecs.npy")
 labels = np.load("wordvec_labels.npy")
-data = data[:, 0:2000, :]
+test = np.load("testvecs.npy")
+test_labels = np.load("test_labels.npy")
+
+data = data[:, 0:100, :]
 print("End Data Processing")
-lstm = LSTM(train = data, train_labels = labels)
+
+lstm = LSTM(train = data, train_labels = labels, test = test, test_labels = test_labels)
 lstm.train_op()

get_wordvecs.py
@@ -55,18 +55,14 @@ for path in paths:
             if word:
                 song_list[song_index].append(word)
 
-# Find max length of a song
-maxl = 0
-for song in song_list:
-    if len(song) > maxl:
-        maxl = len(song)
+# Only use first 100 wors of song
 song_vecs = []
-labels = np.array([i // 100 for i in range(500)])
+labels = np.array([i // 800 for i in range(4000)])
 for num in range(5):
     print(num)
-    for song in song_list[num*1000: num *1000 + 100]:
+    for song in song_list[num*1000: num *1000 + 800]:
         song_seq = []
-        for i in range(maxl):
+        for i in range(100):
             if i > len(song) - 1:
                 song_seq.append(np.zeros(300))
             else:
@@ -76,6 +72,27 @@ for num in range(5):
                     song_seq.append(np.zeros(300))
         song_vecs.append(np.array(song_seq))
 gc.collect()
+
 song_vecs = np.array(song_vecs)
 np.save("wordvecs.npy", song_vecs)
 np.save("wordvec_labels.npy", labels)
+
+test_labels = np.array([i // 100 for i in range(500)])
+test_vecs = []
+for num in range(5):
+    print(num)
+    for song in song_list[(num + 1) * 800: (num + 1) * 800 + 100]:
+        song_seq = []
+        for i in range(100):
+            if i > len(song) - 1:
+                song_seq.append(np.zeros(300))
+            else:
+                try:
+                    song_seq.append(model[song[i]])
+                except:
+                    song_seq.append(np.zeros(300))
+        test_vecs.append(np.array(song_seq))
+gc.collect()
+test_vecs = np.array(test_vecs)
+np.save("testvecs.npy", test_vecs)
+np.save("test_labels.npy", test_labels)